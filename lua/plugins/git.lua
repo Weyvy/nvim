bd5eac2d81580a5d9e6f1fd21f0361a587b9d5e1
weyvy <meyvy.work@gmail.com>
2025-12-25 06:00:13 +0330

    only telescope remains

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,13 +2,47 @@ return {
     {
         'lewis6991/gitsigns.nvim',
         opts = {
-        signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
         },
+    },
+    {
+        "tpope/vim-fugitive",
+        config = function(_, opts)
+            -- Helpful keymaps for Git operations
+            vim.keymap.set('n', '<leader>gs', ':Git<CR>')
+            vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>')
+            vim.keymap.set('n', '<leader>gc', ':Git commit<CR>')
+            vim.keymap.set('n', '<leader>gb', ':Git blame<CR>')
+            vim.keymap.set('n', '<leader>gm', ':Git mergetool<CR>')
+            vim.keymap.set('n', '<leader>gr', ':Gread!<CR>', { desc = 'Git restore file (HEAD)' })
+
+            -- Improve diff experience
+            vim.opt.diffopt:append('algorithm:patience')
+            vim.opt.diffopt:append('indent-heuristic')
+        end
+    },
+    {
+        "NeogitOrg/neogit",
+        lazy = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",  -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",              -- optional
+            "nvim-mini/mini.pick",           -- optional
+            "folke/snacks.nvim",             -- optional
         },
-  },
+        cmd = "Neogit",
+        keys = {
+            { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+        }
+    }
 }
--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,17 +1,33 @@
 return {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-      "kevinhwang91/promise-async",
-    },
-    config = function()
-      local ufo = require("ufo")
-      ufo.setup({
-        provider_selector = function(_, _, _)
-          return { "treesitter", "indent" }
-        end,
-      })
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
+  config = function()
+    local ufo = require("ufo")
+    ufo.setup({
+      provider_selector = function(_, _, _)
+        return { "treesitter", "indent" }
+      end,
+      preview = {
+        mappings = {
+          scrollU = '<C-u>',
+          scrollD = '<C-d>',
+          jumpTop = '[',
+          jumpBot = ']'
+        }
+      },
+    })
 
-      vim.keymap.set('n', 'zR', ufo.openAllFolds)
-      vim.keymap.set('n', 'zM', ufo.closeAllFolds)
-    end,
+    vim.keymap.set('n', 'zR', ufo.openAllFolds, { desc = "Open all folds" })
+    vim.keymap.set('n', 'zM', ufo.closeAllFolds, { desc = "Close all folds" })
+    vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+    vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+    vim.keymap.set('n', 'K', function()
+      local winid = require('ufo').peekFoldedLinesUnderCursor()
+      if not winid then
+        vim.lsp.buf.hover()
+      end
+    end)
+  end,
 }
