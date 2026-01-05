49cdb1a158b98440895f6a2c589d0351c56e4b8c
m.eyvazi <mohamadrezaeivazi@gmail.com>
2026-01-05 19:21:08 +0330

    added to doc

--- guide.md
+++ guide.md
@@ -33,3 +33,4 @@
 # Deps
 - ripgrep
 - fd
+- xclip
--- lua/configs/post_setup.lua
+++ lua/configs/post_setup.lua
@@ -3,3 +3,4 @@ vim.cmd.colorscheme("ayu-dark")
 
 vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#2f344d", bold = true })
 vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#ffcc66", bold = true })
+
