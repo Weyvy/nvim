8fa86c6e0d172ce29f18c4cb31ed628257c29579
eyvazi <meyvy.work@gmail.com>
2026-06-27 08:20:05 +0200

    add center on vertical motions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -25,3 +25,22 @@ vim.keymap.set("n", "gl", vim.diagnostic.open_float, {
 })
 
 vim.keymap.set("n", "<Tab>", "<C-^>", { desc = "Alternate buffer" })
+
+-- Center cursor after vertical navigation
+vim.keymap.set("n", "j", "jzz", { desc = "Move down and center" })
+vim.keymap.set("n", "k", "kzz", { desc = "Move up and center" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up and center" })
+
+vim.keymap.set("n", "<C-f>", "<C-f>zz", { desc = "Page down and center" })
+vim.keymap.set("n", "<C-b>", "<C-b>zz", { desc = "Page up and center" })
+
+vim.keymap.set("n", "G", "Gzz", { desc = "Go to end and center" })
+vim.keymap.set("n", "gg", "ggzz", { desc = "Go to top and center" })
+
+vim.keymap.set("n", "{", "{zz", { desc = "Previous paragraph and center" })
+vim.keymap.set("n", "}", "}zz", { desc = "Next paragraph and center" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result and center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result and center" })
