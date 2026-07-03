0840e6e627b0b1c4aa5d03b70b61b11409609260
eyvazi <meyvy.work@gmail.com>
2026-07-03 20:10:58 +0200

    disable copilot

--- lua/plugin/auto_complete.lua
+++ lua/plugin/auto_complete.lua
@@ -73,7 +73,7 @@ require("blink.cmp").setup({
 
 	sources = {
 		default = {
-			"copilot",
+			-- "copilot",
 			"lsp",
 			"path",
 			"buffer",
@@ -111,11 +111,7 @@ require("blink.cmp").setup({
 					enable_in_context = function()
 						local curpos = vim.api.nvim_win_get_cursor(0)
 
-						local captures = vim.treesitter.get_captures_at_pos(
-							0,
-							curpos[1] - 1,
-							curpos[2] - 1
-						)
+						local captures = vim.treesitter.get_captures_at_pos(0, curpos[1] - 1, curpos[2] - 1)
 
 						local in_spell_capture = false
 
@@ -179,3 +175,4 @@ require("blink.cmp").setup({
 		enabled = true,
 	},
 })
+
